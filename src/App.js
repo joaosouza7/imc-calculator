c337570bf058e8e1385af2d5afab54415698c7db
Joao Souza <joaoosouza07@gmail.com>
2022-01-31 21:17:08 -0300

    Static page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
+import { useState } from 'react';
+import './app.css';
 
 export default function App() {
+
+  const [peso, setPeso] = useState(''); // Armazena o peso digitado no input
+  const [altura, setAltura] = useState(''); // Armazena a altura digitada no input
+  const [mensagem, setMensagem] = useState(''); // Armazena a mensagem declarada no IMC
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(imc < 18.6){
+      document.getElementById('msg').style.color = '#dc3545';
+      document.getElementById('msg').style.border = '1px solid';
+      setMensagem('Você está abaixo do peso! Seu IMC é: ' + imc.toFixed(2));
+    }else if(imc >= 18.6 && imc < 24.9){
+      document.getElementById('msg').style.color = '#198754';
+      document.getElementById('msg').style.border = '1px solid';
+      setMensagem('Peso Ideal! Seu IMC é: ' + imc.toFixed(2));
+    }else if(imc >= 24.9 && imc < 34.9){
+      document.getElementById('msg').style.color = '#ffc107';
+      document.getElementById('msg').style.border = '1px solid';
+      setMensagem('Você está levemente acima do peso! Seu IMC é: ' + imc.toFixed(2));
+    }else if(imc > 34.9){
+      document.getElementById('msg').style.color = '#dc3545';
+      document.getElementById('msg').style.border = '1px solid';
+      setMensagem('Cuidado, risco de obesidade! Seu IMC é: ' + imc.toFixed(2));
+    }
+  }
+
   return(
-    <div>
+    <div className="app">
       <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC?</span>
+
+      <div className="area-input">
+        <input 
+          type="text"
+          placeholder="Peso (kg)"
+          value={peso}
+          onChange={ (e) => setPeso(e.target.value) }
+        />
+
+        <input 
+          type="text"
+          placeholder="Altura (cm)"
+          value={altura}
+          onChange={ (e) => setAltura(e.target.value) }
+        />
+
+        <button onClick={calcularIMC}>Calcular</button>
+      </div>
+
+      <h3 id="msg">{mensagem}</h3>
     </div>
   );
 }
